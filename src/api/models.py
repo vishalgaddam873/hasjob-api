93f4d04d0de15fce839358a01d2f51906c379093
Vishal Gaddam <41083456+vishalgaddam873@users.noreply.github.com>
2018-10-08 22:30:17 +0530

    Update models.py

--- src/api/models.py
+++ src/api/models.py
@@ -7,30 +7,30 @@ class JobDetail(models.Model):
 	JOBLOCATION = (('ANYWHERE','Anywhere'),('BANGLORE','Banglore, IN'),('CHENNAI','Channai, IN'),('MUMBAI','Mumbai, IN'),('PUNE','Pune, IN'))
 	job_title 			= models.CharField(max_length = 100)
 	job_type  			= models.CharField(max_length=50,choices= JOBTYPE, blank=True,null=False)
-	job_category		= models.CharField(max_length=50,choices=JOBCATEGORY, null=False ,blank=True)
-	job_location 		= models.CharField(max_length=50,choices=JOBLOCATION, null=True, blank=True)
-	job_description 	= models.TextField(null=False,blank=True)
+	job_category			= models.CharField(max_length=50,choices=JOBCATEGORY, null=False ,blank=True)
+	job_location 			= models.CharField(max_length=50,choices=JOBLOCATION, null=True, blank=True)
+	job_description 		= models.TextField(null=False,blank=True)
 	salary				= models.PositiveIntegerField(validators=[MaxValueValidator(9999999999,message='Enter correct salary')])
-	skill_requirements 	= models.TextField(null=True, blank=True)
+	skill_requirements 		= models.TextField(null=True, blank=True)
 	job_preks 			= models.TextField(null=True,blank=True)
-	company_name 		= models.CharField(max_length=80)
-	date_of_post 		= models.DateField(auto_now_add=True,null=False)
-	Employee_Detail 	= models.TextField(null=False,blank=False)
+	company_name 			= models.CharField(max_length=80)
+	date_of_post 			= models.DateField(auto_now_add=True,null=False)
+	Employee_Detail 		= models.TextField(null=False,blank=False)
 
 	def __str__(self):
 		return self.job_title
 
 class User(models.Model):
-	GENDER 					= (('MALE','Male'),('FEMALE','Female'),('OTHER','Other'))
-	first_name 				= models.CharField(max_length=50)
-	last_name 				= models.CharField(max_length=50)
-	dob						= models.DateField(null=False,blank=True)
-	gender					= models.CharField(max_length=50, choices=GENDER,null=False,blank=True)
+	GENDER 				= (('MALE','Male'),('FEMALE','Female'),('OTHER','Other'))
+	first_name 			= models.CharField(max_length=50)
+	last_name 			= models.CharField(max_length=50)
+	dob				= models.DateField(null=False,blank=True)
+	gender				= models.CharField(max_length=50, choices=GENDER,null=False,blank=True)
 	contact_number 			= models.PositiveIntegerField(null=False,validators=[MaxValueValidator(999999999999,message='Please enter correct Mobile number')])
-	email_id				= models.EmailField()
-	address					= models.TextField(null=False,max_length=500,blank=True)
-	experience				= models.CharField(max_length=30, default='Fresher')
-	resume_url				= models.URLField(null=False)
+	email_id			= models.EmailField()
+	address				= models.TextField(null=False,max_length=500,blank=True)
+	experience			= models.CharField(max_length=30, default='Fresher')
+	resume_url			= models.URLField(null=False)
 	proffession 			= models.CharField(null=False,max_length=100)
 	current_or_pervious_company_name = models.CharField(max_length=50,null=True)
 
@@ -39,9 +39,9 @@ class User(models.Model):
 
 
 class JobApplications(models.Model):
-	job_id 					= models.ForeignKey(JobDetail,on_delete= 'CASCADE'),
-	user_id 				= models.ForeignKey(User,on_delete= 'CASCADE'),
-	expected_salary 		= models.PositiveIntegerField(validators=[MaxValueValidator(9999999999,message='Please Check you Enter Correct Salary or not')])
+	job_id 			= models.ForeignKey(JobDetail,on_delete= 'CASCADE'),
+	user_id 		= models.ForeignKey(User,on_delete= 'CASCADE'),
+	expected_salary 	= models.PositiveIntegerField(validators=[MaxValueValidator(9999999999,message='Please Check you Enter Correct Salary or not')])
 	job_related_experince 	= models.TextField(null=True,blank=True)
 
 	def __str__(self):
